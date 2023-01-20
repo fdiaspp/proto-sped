56b418ffa5f17baafe79982870f8835dda6eb5a8
Felipe Pereira <fdiaspp@gmail.com>
2023-01-20 14:12:20 -0300

    novas alterações

--- utils/sped2protobuf/main.py
+++ utils/sped2protobuf/main.py
@@ -2,7 +2,7 @@ import sys
 import pathlib
 
 
-if not pathlib.Path("../../taget/python/sped_pb2.py").exists():
+if not pathlib.Path("../../target/python/sped_pb2.py").exists():
     print("Protobuf schema não compilado na versão Python.\n"
            "Na raiz do projeto, execute o seguinte comando\n"
            "$ cd ../../ && make build-python && cd -")
@@ -29,13 +29,34 @@ def get_register_type(line: str):
 
 
 def get_message_schema(message_type):
-    return [field for field in message_type.DESCRIPTOR.fields_by_name]
+    
+    # fonte: https://googleapis.dev/python/protobuf/latest/google/protobuf/descriptor.html#google.protobuf.descriptor.FieldDescriptor.TYPE_BOOL
+    map_types = {
+        9: str,
+        5: int,
+        3: int, 
+        2: float,
+        8: bool}
+    
+    types = {f.name: map_types[f.type] for f in message_type.DESCRIPTOR.fields}
+    schema = [field for field in message_type.DESCRIPTOR.fields_by_name]
+    
+    return schema, types
 
 
 def process_line(line: str, message_schema: list):
     data = line.split("|")
     zipped_data = dict(zip(message_schema, data[:-1]))
     return zipped_data
+
+
+def transform_data(data: dict, fields_expected_type: dict):    
+    new_data = {}
+    for key in data:
+        new_value = fields_expected_type[key](data[key])
+        new_data.update({key: new_value})
+        
+    return new_data
 
 
 def main(path):    
@@ -47,8 +68,9 @@ def main(path):
                 break
 
             message_type = get_register_type(line)
-            message_schema = get_message_schema(message_type)
+            message_schema, message_types = get_message_schema(message_type)
             data = process_line(line, message_schema)
+            data = transform_data(data, message_types)
             yield message_type(**data) 
 
                
